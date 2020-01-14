d2f3c044dcdee8fe2135fe25924d11cb44156865
VashistaGande <vashistagande@gmail.com>
2020-01-14 14:15:59 -0500

    Update AppDelegate.swift

--- Morning Assist/AppDelegate.swift
+++ Morning Assist/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//        GMSPlacesClient.provideAPIKey("AIzaSyDQmREGRM4gJ5WcEUiaLubo-6xa-uvsKOw")
+//        GMSPlacesClient.provideAPIKey("_______")
         
         // Override point for customization after application launch.
         return true
